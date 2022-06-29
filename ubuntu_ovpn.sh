1f9e05fd4d989e992dfc6d377fda675a1d18b119
nishatvpn <92996099+nishatvpn@users.noreply.github.com>
2022-06-30 01:33:05 +0200

    Update ubuntu_ovpn.sh

--- ubuntu_ovpn.sh
+++ ubuntu_ovpn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='209.159.152.66';
-USER='crypticv_freedata';
-PASS='@@@@F1r3n3t';
-DBNAME='crypticv_freedata';
+HOST='174.138.183.243';
+USER='abolirez_arman';
+PASS='abolirez_arman';
+DBNAME='abolirez_arman';
 
 install_require()
 {
